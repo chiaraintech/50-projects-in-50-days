ef11147e5f2395edbb58256009bce6e57b435781
chiaraintech <chiaraintech@gmail.com>
2021-04-01 15:47:34 +0100

    Seventh project committed.

--- 6. scroll-animation/script.js
+++ 6. scroll-animation/script.js
@@ -5,6 +5,7 @@ function checkBoxes() {
     const triggerBottom = window.innerHeight / 5 * 4;
     boxes.forEach(box => {
         const boxTop = box.getBoundingClientRect().top;
+        console.log(boxTop)
         if (boxTop < triggerBottom) {
             box.classList.add('show')
         } else {
