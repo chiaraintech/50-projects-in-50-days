684cb330883bb29c80ad01736a358a7830db1f25
chiaraintech <chiaraintech@gmail.com>
2021-04-11 16:57:13 +0100

    Main JS functionality and logic added.

--- 17. movie-app/script.js
+++ 17. movie-app/script.js
@@ -1,7 +1,7 @@
 const API_KEY = '7dccf571a7a8ecb73c7167c342b4a901';
-const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query="`
+const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`;
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query="`;
 
 const main = document.getElementById('main');
 const form = document.getElementById('form');
@@ -12,8 +12,7 @@ getMovies(API_URL);
 async function getMovies(url) {
     const res = await fetch(url);
     const data = await res.json();
-
-    console.log(data.results);
+    showMovies(data.results);
 }
 
 
@@ -29,3 +28,37 @@ form.addEventListener('submit', (e) => {
         window.location.reload();
     }
 });
+
+function showMovies(movies) {
+    main.innerHTML = '';                                                            //Clearing the value of main in preparation to the searched value.
+
+    movies.forEach(movie => {
+        const { title, poster_path, vote_average, overview } = movie;
+
+        const movieDiv = document.createElement('div');
+        movieDiv.classList.add('movie');
+
+        movieDiv.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        `
+        main.appendChild(movieDiv);
+    });
+};
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green';
+    } else if (vote >= 5) {
+        return 'orange';
+    } else {
+        return 'red';
+    }
+};
