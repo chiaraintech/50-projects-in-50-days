57198813c0a92d7412e35c9ddb66d04fbe115090
chiaraintech <chiaraintech@gmail.com>
2021-04-12 20:53:00 +0100

    Added comments with explanations.

--- 18. image-slider/script.js
+++ 18. image-slider/script.js
@@ -3,21 +3,21 @@ const slides = document.querySelectorAll('.slide');
 const leftButton = document.getElementById('left');
 const rightButton = document.getElementById('right');
 
-let activeSlide = 0;
+let activeSlide = 0;                                                                //First image initalised at index 0.
 
 rightButton.addEventListener('click', () => {
-    activeSlide++;
-    if (activeSlide > slides.length -1) {
-        activeSlide = 0
+    activeSlide++;                                                                  //Increment the index of the slides by 1 if I click on the right arrow,
+    if (activeSlide > slides.length -1) {                                           //If the index is bigger than the amount of slides,
+        activeSlide = 0;                                                            //Then reset the index to 0.
     }
-    setBgToBody();
+    setBgToBody();                                                              
     setActiveSlide();
 });
 
 leftButton.addEventListener('click', () => {
-    activeSlide--;
-    if (activeSlide < 0) {
-        activeSlide = slides.length - 1
+    activeSlide--;                                                                  //Decrement the index of the slides by 1 if I click on the left arrow,
+    if (activeSlide < 0) {                                                          //If the index is smaller than the starting index of the slides,
+        activeSlide = slides.length - 1                                             //Then make the next active slide the last one of the array.
     }
     setBgToBody();
     setActiveSlide();
@@ -26,7 +26,7 @@ leftButton.addEventListener('click', () => {
 setBgToBody();
 
 function setBgToBody() {
-    body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage;         //Set the background image according to the URL of the index of the image in the slide.
 };
 
 function setActiveSlide() {
