c61e26a20ab8b64bd835dc2f56c68ff43ae5ff73
chiaraintech <chiaraintech@gmail.com>
2021-04-13 16:16:34 +0100

    Time Functionality added using JS new Date() API.

--- 19. theme-clock/script.js
+++ 19. theme-clock/script.js
@@ -3,43 +3,46 @@ const minuteEl = document.querySelector('.minute');
 const secondEl = document.querySelector('.second');
 const timeEl = document.querySelector('.time');
 const dateEl = document.querySelector('.date');
-const toggleButton = document.querySelector('.toggle');
+const toggle = document.querySelector('.toggle');
 
-const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-toggleButton.addEventListener('click', (e) => {
+toggle.addEventListener('click', (e) => {
     const html = document.querySelector('html');
     if (html.classList.contains('dark')) {
         html.classList.remove('dark');
-        e.target.innerHTML = 'Dark Mode';
+        e.target.innerHTML = 'Dark mode';
     } else {
         html.classList.add('dark');
-        e.target.innerHTML = 'Light Mode';
-    }
+        e.target.innerHTML = 'Light mode';
+    };
 });
 
-//From Stackoverflow
-const scale = (num, in_min, in_max, out_min, out_max) => {
-    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-};
 
 function setTime() {
-    const timeData = new Date();
-    const monthData = time.getMonth();
-    const dateData = time.getDay();
+    const time = new Date();
+    const month = time.getMonth();
+    const day = time.getDay();
+    const date = time.getDate();
     const hours = time.getHours();
-    const hoursForClock = hours * 12;
+    const hoursForClock = hours >= 13 ? hours % 12 : hours;
     const minutes = time.getMinutes();
     const seconds = time.getSeconds();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
 
-    hourEl.getElementsByClassName.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`;
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`;
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`;
 
-    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
-    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`;
+    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
+}
 
-    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
-    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
+// StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 setTime();
+setInterval(setTime, 1000);
