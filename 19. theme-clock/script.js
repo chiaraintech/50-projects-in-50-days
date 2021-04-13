af49454a811a4663e3074de29a71b1a6013dc4a7
chiaraintech <chiaraintech@gmail.com>
2021-04-13 16:11:52 +0100

    CSS more specific.

---
+++ 19. theme-clock/script.js
@@ -0,0 +1,45 @@
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const secondEl = document.querySelector('.second');
+const timeEl = document.querySelector('.time');
+const dateEl = document.querySelector('.date');
+const toggleButton = document.querySelector('.toggle');
+
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+toggleButton.addEventListener('click', (e) => {
+    const html = document.querySelector('html');
+    if (html.classList.contains('dark')) {
+        html.classList.remove('dark');
+        e.target.innerHTML = 'Dark Mode';
+    } else {
+        html.classList.add('dark');
+        e.target.innerHTML = 'Light Mode';
+    }
+});
+
+//From Stackoverflow
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+};
+
+function setTime() {
+    const timeData = new Date();
+    const monthData = time.getMonth();
+    const dateData = time.getDay();
+    const hours = time.getHours();
+    const hoursForClock = hours * 12;
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+
+    hourEl.getElementsByClassName.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+
+    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
+}
+
+setTime();
