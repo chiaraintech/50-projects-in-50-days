3552ce7ab6968707221969e199f26cec13dcfe5d
chiaraintech <chiaraintech@gmail.com>
2021-04-16 08:19:14 +0100

    wow! I am able to draw on the canvas just using event listeners and mouse events.

--- 22. drawing-app/script.js
+++ 22. drawing-app/script.js
@@ -1,16 +1,40 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const size = 20;
+let isPressed = false;
 const color = 'black';
 let x;
 let y;
+
+canvas.addEventListener('mousedown', (e) => {
+    isPressed = true;
+    x = e.offsetX;
+    y = e.offsetY;
+});
+
+canvas.addEventListener('mouseup', () => {
+    isPressed = false;
+    x = undefined;
+    y = undefined;
+});
+
+canvas.addEventListener('mousemove', (e) => {
+    if (isPressed) {
+        const x2 = e.offsetX;
+        const y2 = e.offsetY;
+        drawCircle(x2, y2);
+        drawLine(x, y, x2, y2);
+        x = x2;
+        y = y2;
+    }
+});
 
 function drawCircle(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, size, 0, Math.PI * 2, true); // Outer circle
     ctx.fillStyle = color;
     ctx.fill();
-}
+};
 
 function drawLine(x1, y1, x2, y2) {
     ctx.beginPath();
@@ -19,7 +43,4 @@ function drawLine(x1, y1, x2, y2) {
     ctx.strokeStyle = color;
     ctx.lineWidth = size;
     ctx.stroke()
-}
-
-drawCircle(100, 200)
-drawLine(300, 300, 500, 500)
+};
