adcd1a4b42fe4dd332a0cc669ae904bb4d005a61
chiaraintech <chiaraintech@gmail.com>
2021-04-20 17:05:04 +0100

    Just manipulating some coloirs

--- 27. toast-notification/script.js
+++ 27. toast-notification/script.js
@@ -1,5 +1,6 @@
 const button = document.getElementById('button');
 const toasts = document.getElementById('toasts');
+const types = ['info', 'success', 'error'];
 
 const messages = [
     'You are amazing!',
@@ -10,13 +11,22 @@ const messages = [
 
 button.addEventListener('click', () => createNotification())
 
-function createNotification() {
+function createNotification(message = null, type = null) {
     const notif = document.createElement('div');
     notif.classList.add('toast');
-    notif.innerText = getRandomMessage();
-    toasts.appendChild(notif)
+    notif.classList.add(type ? type : getRandomType());
+    notif.innerText = message ? message : getRandomMessage();
+    toasts.appendChild(notif);
+
+    setTimeout(() => {
+        notif.remove()
+    }, 3000)
 }
 
 function getRandomMessage() {
     return messages[Math.floor(Math.random() * messages.length)]
+}
+
+function getRandomType() {
+    return types[Math.floor(Math.random() * types.length)]
 }
