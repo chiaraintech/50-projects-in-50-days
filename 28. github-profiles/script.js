26af4794ce881a13a9d6049dc537cc69987b7147
chiaraintech <chiaraintech@gmail.com>
2021-04-21 13:41:44 +0100

    Repositories' links added.

--- 28. github-profiles/script.js
+++ 28. github-profiles/script.js
@@ -7,10 +7,20 @@ async function getUser(username) {
     try {
         const {data} = await axios(APIURL + username);
         createUserData(data);
+        getRepos(username);
     } catch(err) {
         if(err.response.status == 404) {
         createErrorCard('No profile with this username.')
         }
+    }
+}
+
+async function getRepos(username) {
+    try {
+        const {data} = await axios(APIURL + username + '/repos?sort=created');
+        addReposToCard(data);
+    } catch(err) {
+        createErrorCard('Problem fetching repos.')
     }
 }
 
@@ -26,11 +36,7 @@ function createUserData(user) {
                     <li>${user.following} <strong>Following</strong></li>
                     <li>${user.public_repos} <strong>Repos</strong></li>
                 </ul>
-            <div id="repos">
-                <a href="#" class="repo">Repo 1</a>
-                <a href="#" class="repo">Repo 2</a>
-                <a href="#" class="repo">Repo 3</a>
-            </div>
+            <div id="repos"></div>
             </div>
         </div>
     `
@@ -47,6 +53,21 @@ form.addEventListener('submit', (e) => {
         search.value = ''
     }
 });
+
+function addReposToCard(repos) {
+    const reposElement = document.getElementById('repos');
+    
+    repos
+    .slice(0,10)
+    .forEach(repo => {
+        const repoElement = document.createElement('a');
+        repoElement.classList.add('repo');
+        repoElement.href = repo.html_url;
+        repoElement.target = '_blank';
+        repoElement.innerText = repo.name;
+        reposElement.appendChild(repoElement);
+    })
+}
 
 function createErrorCard(message) {
     const cardHTML = `
