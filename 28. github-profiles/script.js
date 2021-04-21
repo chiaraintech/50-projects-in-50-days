41b77acfa8107e3c2a2e20a76470516bf550ab15
chiaraintech <chiaraintech@gmail.com>
2021-04-21 13:28:59 +0100

    Error handling finalised.

--- 28. github-profiles/script.js
+++ 28. github-profiles/script.js
@@ -1,19 +1,40 @@
 const APIURL = 'https://api.github.com/users/';
 const form = document.getElementById('form');
 const search = document.getElementById('search');
+const main = document.getElementById('main');
 
 async function getUser(username) {
-
     try {
-
-        const {data} = await axios(APIURL + username)
-        console.log(data)
-
+        const {data} = await axios(APIURL + username);
+        createUserData(data);
     } catch(err) {
-        console.log(err)
+        if(err.response.status == 404) {
+        createErrorCard('No profile with this username.')
+        }
     }
+}
 
-    
+function createUserData(user) {
+    const cardHTML = `
+    <div class="card">
+                <img src="${user.avatar_url}" alt="avatar" class="avatar">
+            <div class="user-info">
+                <h2>${user.name}</h2>
+                <p>${user.bio}</p>
+                <ul>
+                    <li>${user.followers} <strong>Followers</strong></li>
+                    <li>${user.following} <strong>Following</strong></li>
+                    <li>${user.public_repos} <strong>Repos</strong></li>
+                </ul>
+            <div id="repos">
+                <a href="#" class="repo">Repo 1</a>
+                <a href="#" class="repo">Repo 2</a>
+                <a href="#" class="repo">Repo 3</a>
+            </div>
+            </div>
+        </div>
+    `
+    main.innerHTML = cardHTML;
 }
 
 form.addEventListener('submit', (e) => {
@@ -25,4 +46,13 @@ form.addEventListener('submit', (e) => {
         getUser(user)
         search.value = ''
     }
-})
+});
+
+function createErrorCard(message) {
+    const cardHTML = `
+    <div class="card">
+        <h1>${message}</h1>
+    </div>
+    `
+    main.innerHTML = cardHTML
+}
