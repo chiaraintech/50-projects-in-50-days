18d196832561ceaa2c18cc300ac372b9d7e1ea54
chiaraintech <chiaraintech@gmail.com>
2021-04-28 13:54:33 +0100

    Main functionality done.

--- 33. notes-app/script.js
+++ 33. notes-app/script.js
@@ -1,6 +1,7 @@
 const addBtn = document.getElementById('add');
 
-addBtn.addEventListener('click', () => addNewNote())
+
+addBtn.addEventListener('click', () => addNewNote());
 
 function addNewNote(text = '') {
     const note = document.createElement('div');
@@ -15,5 +16,27 @@ function addNewNote(text = '') {
     <textarea class="${text ? "hidden" : ""}"></textarea>
     `
 
-    document.body.appendChild(note)
+    const editBtn = note.querySelector('.edit');
+    const deleteBtn = note.querySelector('.delete');
+    const main = note.querySelector('.main');
+    const textArea = note.querySelector('textarea');
+
+    textArea.value = text;
+    main.innerHTML = marked(text);
+
+    deleteBtn.addEventListener('click', () => {
+        note.remove();
+    });
+
+    editBtn.addEventListener('click', () => {
+        main.classList.toggle('hidden');
+        textArea.classList.toggle('hidden');
+    });
+
+    textArea.addEventListener('input', (e) => {
+        const { value } = e.target;
+        main.innerHTML = marked(value);
+    });
+
+    document.body.appendChild(note);
 }
