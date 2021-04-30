4d0c754acc97b7c6119df7859ac36165a544e223
chiaraintech <chiaraintech@gmail.com>
2021-04-30 11:55:31 +0100

    Carousel functionality added alongside controls from the buttons.

---
+++ 35. image-carousel/script.js
@@ -0,0 +1,39 @@
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+const container = document.getElementById('imgs');
+const image = document.querySelectorAll('#imgs img');
+
+let index = 0;
+
+let interval = setInterval(run, 2000);
+
+function run() {
+    index++;
+    changeImage();
+}
+
+function changeImage() {
+    if (index > image.length -1) {
+        index = 0
+    } else if (index < 0) {
+        index = image.length - 1
+    }
+    container.style.transform = `translateX(${-index * 500}px)`;
+}
+
+function resetInterval() {
+    clearInterval(interval);
+    interval = setInterval(run, 2000);
+}
+
+rightBtn.addEventListener('click', () => {
+    index++;
+    changeImage();
+    resetInterval()
+});
+
+leftBtn.addEventListener('click', () => {
+    index--;
+    changeImage();
+    resetInterval();
+});
